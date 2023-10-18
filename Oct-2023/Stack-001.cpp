19ccc64d84bc875b757cca803bbcacb5dbc19c96
lfnl <3096859484@qq.com>
2023-10-18 22:33:52 +0800

    Stack的实现

---
+++ Oct-2023/Stack-001.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class Stack{
+private:
+    static const int MAX_SIZE=100;
+    int data[MAX_SIZE];
+    int topIndex;
+
+public:
+    Stack():topIndex(-1){}
+
+    // 将元素压入栈
+    void push(int value){
+        if(topIndex<MAX_SIZE-1){
+            data[++topIndex]=value;
+        }else{
+            cout<<"Stack overflow"<<endl;
+        }
+    }
+
+    // 弹出栈顶元素
+    void pop(){
+        if(!isEmpty()){
+            --topIndex;
+        }else{
+            cout<<"Stack is empty"<<endl;
+        }
+    }
+
+    // 获取栈顶元素
+    int top(){
+        if(!isEmpty()){
+            return data[topIndex];
+        }
+        return -1;
+    }
+
+    // 检查栈是否为空
+    bool isEmpty(){
+        return topIndex==-1;
+    }
+
+    // 获取栈的大小
+    int size(){
+        return topIndex+1;
+    }
+};
+
+int main(){
+    Stack myStack;
+    myStack.push(1);
+    myStack.push(2);
+    myStack.push(3);
+
+    cout<<"Top of the stack: "<<myStack.top()<<endl;
+
+    myStack.pop();
+    cout<<"Top of the stack after pop: "<<myStack.top()<<endl;
+
+    cout<<myStack.size()<<endl;
+    return 0;
+}
