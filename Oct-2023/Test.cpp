8d39e2f647053031b3fa2847720d7f1f6441fda6
lfnl <3096859484@qq.com>
2023-11-25 20:30:14 +0800

    校赛日

--- Oct-2023/Test.cpp
+++ Oct-2023/Test.cpp
@@ -1,8 +1,31 @@
 #include<iostream>
-#include<stack>
+
 using namespace std;
 
 int main(){
-    stack<int> myStack;
-    myStack.empty();
+    int array[12];
+    for(int i=0;i<12;i++){
+        cin>>array[i];
+    }
+
+    int remain=0;
+    int mom=0;
+    for(int i=0;i<12;i++){
+        if(array[i]<=300+remain){
+            int last=300+remain-array[i];
+            if(last/100>=1){
+                mom+=last/100;
+                remain=last%100;
+            }else{
+                remain=last;
+            }
+         
+        }else{
+            cout<<"-"<<i+1<<endl;
+            return 0;
+        }
+    }
+
+    cout<<remain+mom*120<<endl;
+    return 0;
 }
