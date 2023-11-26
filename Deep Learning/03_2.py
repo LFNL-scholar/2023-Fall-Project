9eff0913864abd1be50a387b2ac2c755a5f89b22
lfnl <3096859484@qq.com>
2023-11-26 14:44:37 +0800

    前向处理

--- Deep Learning/03_2.py
+++ Deep Learning/03_2.py
@@ -1,2 +1,45 @@
 import numpy as np
 
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+# 从输入层到第一层的信号传递
+X=np.array([1.0,0.5])
+W1=np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+B1=np.array([0.1,0.2,0.3])
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1=np.dot(X,W1)+B1
+Z1=sigmoid(A1)
+
+print(A1)
+print(Z1)
+
+# 从第一层到第二层的信号传递
+W2=np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+B2=np.array([0.1,0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2=np.dot(Z1,W2)+B2
+Z2=sigmoid(A2)
+
+print(A2)
+print(Z2)
+
+# 第二层到输出层的信号传递
+def identity_function(x):
+    return x
+
+W3=np.array([[0.1,0.3],[0.2,0.4]])
+B3=np.array([0.1,0.2])
+
+A3=np.dot(Z2,W3)+B3
+Y=identity_function(A3)
+
+print(Y)
