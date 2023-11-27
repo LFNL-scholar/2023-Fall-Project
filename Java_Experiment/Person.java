3fcfef4914a5a39b35e58a3e1dbbc1ab647a004f
LFNL-scholar <106868999+LFNL-scholar@users.noreply.github.com>
2023-11-27 18:03:18 +0800

    Create Person.java
    111

---
+++ Java_Experiment/Person.java
@@ -0,0 +1,47 @@
+package Java_Experiment;
+
+public class Person {
+    public String name;
+    protected int age;
+    private String address;
+
+    public void introduce(){
+        System.out.println("Hi, I'm "+name+". I am "+age+" years old.");
+    }
+    protected void updateAddress(String newAddress){
+        this.address=newAddress;
+        System.out.println("Address updated to: "+address);
+    }
+    private void privateMethod(){
+        System.out.println("This is a private method.");
+    }
+}
+
+class Student extends Person {
+    public void introduce(){
+        System.out.println("Hi, I'm a student named "+name+". I am "+age+" years old.");
+    }
+    public void changeAddress(String newAdress){
+        updateAddress(newAdress);
+    }    
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        Person person=new Person();
+        person.name="LFNL";
+        person.age=19;
+        person.introduce();
+
+        person.age=26;
+        person.updateAddress("1421 Main St");
+
+        Student student=new Student();
+        student.name="Nancy";
+        student.age=22;
+        student.introduce();
+        student.changeAddress("473 College Ave");
+        
+    }
+}
