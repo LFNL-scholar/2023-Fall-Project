761fa998a8d7f604d23545cc60a05e46b2f12d1e
LFNL-scholar <106868999+LFNL-scholar@users.noreply.github.com>
2023-12-03 18:13:06 +0800

    Update Animal.java

--- Java_Experiment/Animal.java
+++ Java_Experiment/Animal.java
@@ -1,83 +1,79 @@
-
+package Java_Experiment;
 public class Animal {
     private String species;
     private int age;
-    private static int numberOfAnimals = 0;  // 使用static定义静态变量
+    private static int numberOfAnimals=0;
 
     // Constants
-    public static final String SOUND = "Animal makes a sound";  // 使用final定义常量
-
+    public static final String SOUND="Animal makes a sound";
+    
     // Constructors
-    public Animal() {
+    public Animal(){
         numberOfAnimals++;
     }
 
-    public Animal(String species, int age) {
-        this.species = species;
-        this.age = age;
+    public Animal(String species,int age){
+        this.species=species;
+        this.age=age;
         numberOfAnimals++;
     }
 
-    // Methods
-    public void makeSound() {
+    //Methods
+    public void makeSound(){
         System.out.println(SOUND);
     }
 
-    public void eat() {
+    public void eat(){
         System.out.println("The animal is eating.");
     }
 
-    // Overloaded methods
-    public void sleep() {
+    //Overloaded methods
+    public void sleep(){
         System.out.println("The animal is sleeping.");
     }
 
-    public void sleep(int hours) {
-        System.out.println("The animal is sleeping for " + hours + " hours.");
+    public void sleep(int hours){
+        System.out.println("The animal is sleeping for "+hours+" hours.");
     }
 
-    // Getters and Setters
-    public String getSpecies() {
+    //Getters and Setters
+    public String getSpecies(){
         return species;
     }
 
-    public void setSpecies(String species) {
-        this.species = species;
+    public void setSpecies(String species){
+        this.species=species;
     }
 
-    public int getAge() {
+    public int getAge(){
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void steAge(int age){
+        this.age=age;
     }
 
-    public static int getNumberOfAnimals() {
+    public static int getNumberOfAnimals(){
         return numberOfAnimals;
     }
 
-    // Overriding toString() method from Object class
+    //Overriding toString() method from Object class
     @Override
-    public String toString() {
-        return "Animal [species=" + species + ", age=" + age + "]";
+    public String toString(){
+        return "Animal [species="+species+", age="+age+"]";
     }
 
-    // Example of static method
-    public static void printSound() {
+    public static void printSound(){
         System.out.println("Every animal makes a sound.");
     }
 
     public static void main(String[] args) {
-        // 创建Animal类的对象
-        Animal lion = new Animal("Lion", 5);
-        Animal elephant = new Animal("Elephant", 10);
+        Animal lion=new Animal("Lion",5);
+        Animal elephant=new Animal("Elephant",10);
 
-        // 访问对象的属性
-        System.out.println("Lion: " + lion.getSpecies() + ", Age: " + lion.getAge());
-        System.out.println("Elephant: " + elephant.getSpecies() + ", Age: " + elephant.getAge());
+        System.out.println("Lion: "+lion.getSpecies()+", Age: "+lion.getAge());
+        System.out.println("Elephant: "+elephant.getSpecies()+", Age: "+elephant.getAge());
 
-        // 调用对象的方法
         lion.makeSound();
         lion.eat();
         lion.sleep(8);
@@ -86,11 +82,7 @@ public class Animal {
         elephant.eat();
         elephant.sleep();
 
-        // 访问静态变量
-        System.out.println("Number of animals created: " + Animal.getNumberOfAnimals());
-
-        // 调用静态方法
+        System.out.println("NUmber of animals created: "+Animal.getNumberOfAnimals());
         Animal.printSound();
     }
 }
-
