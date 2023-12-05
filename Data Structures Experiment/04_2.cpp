cb725ec190f5fbbb5f295e191bd257d539af37b2
lfnl <3096859484@qq.com>
2023-12-05 23:43:13 +0800

    12/5 evening push. why life is so hard?

--- Data Structures Experiment/04_2.cpp
+++ Data Structures Experiment/04_2.cpp
@@ -1,1 +1,65 @@
-//创建一个无向图（采用邻接表的方式存储）；分别输出从结点0开始的一个深度优先遍历序列和一个广度优先遍历序列。
+//创建一个无向图（采用邻接表的方式存储）；分别输出从结点0开始的一个深度优先遍历序列和一个广度优先遍历序列
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Graph{
+private:
+    int vertices;
+    vector<vector<int>> adjacencyList; // 邻接表
+
+public:
+    Graph(int v):vertices(v),adjacencyList(v) {}
+
+    void addEdge(int start,int end){
+        adjacencyList[start].push_back(end);
+        adjacencyList[end].push_back(start);
+    }
+
+    void addEdges(const vector<vector<int>>& edges){
+        for(const auto& edge : edges){
+            if(edge.size()==2){
+                addEdge(edge[0],edge[1]);
+            }
+        }
+    }
+
+    void DFS(int start,vector<bool>& visited){
+        cout<<start<<" ";
+        visited[start]=true;
+        for(int neighbor:adjacencyList[start]){
+            if(!visited[neighbor]){
+                DFS(neighbor,visited)
+            }
+        }
+    }
+
+    void BFS(int start){
+        vector<bool> visited(vertices,false);
+        queue<int> q;
+        cout<<"BFS Traversal: ";
+        visited[start]=true;
+        q.push(start);
+
+        while(!q.empty()){
+            int current=q.front();
+            cout<<current<<" ";
+            q.pop();
+
+            for(int neighbor:adjacencyList[current]){
+                if(!visited[neighbor]){
+                    visited[neighbor]=true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        cout<<endl;
+    }
+};
+
+int main(){
+    Graph Graph(6);
+
+    return 0;
+}
--- Data Structures Experiment/06_1.cpp
+++ Data Structures Experiment/06_1.cpp
@@ -44,6 +44,38 @@ public:
         ListNode* sorted=NULL;
         ListNode* current=head;
         
+        while(current!=NULL){
+            ListNode* nextNode=current->next;
+            insert(sorted,current);
+            current=nextNode;
+        }
+        head=sorted;
     }
 
+    void display(){
+        ListNode* current=head;
+        while(current!=NULL){
+            cout<<current->data<<" ";
+            current=current->next;
+        }
+        cout<<endl;
+    }
 };
+
+int main(){
+    InsertionSort list;
+    list.insert(5);
+    list.insert(2);
+    list.insert(8);
+    list.insert(1);
+    list.insert(3);
+
+    cout << "Original List: ";
+    list.display();
+    list.sort();
+
+    cout << "Sorted List: ";
+    list.display();
+    
+    return 0;
+}
