8e3174a829a7abf6a31ee9f230a31157d0a2e577
LFNL-scholar <106868999+LFNL-scholar@users.noreply.github.com>
2023-12-07 17:24:52 +0800

    Update 1_Bubble_Sort.cpp

--- Data Structures Experiment/1_Bubble_Sort.cpp
+++ Data Structures Experiment/1_Bubble_Sort.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 #define MAXSIZ 10
 
@@ -27,7 +28,7 @@ void SwapSort(SqList *L){
 //冒泡排序
 void BubbleSort(SqList *L){
     for(int i=1;i<L->length;i++){
-        for(j=L->length-1;j>=i;j--){
+        for(int j=L->length-1;j>=i;j--){
             if(L->arr[j]>L->arr[j+1]){
                 swap(L,j,j+1);
             }
@@ -37,5 +38,17 @@ void BubbleSort(SqList *L){
 
 
 int main(){
+    SqList list;
+    int arr[]={9,1,5,8,3,7,4,6,2};
+    for(int i=0;i<10;i++){
+        list.arr[i]=arr[i-1];
+    }
+    //SwapSort(&list);
+    BubbleSort(&list);
+    for(int i=0;i<10;i++){
+        cout<<list.arr[i]<<" ";
+    }
+    cout<<endl;
 
+    return 0;
 }
