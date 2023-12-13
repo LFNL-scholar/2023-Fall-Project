65297e8d051bd860f3961af84f33e55c66639aa6
LFNL-scholar <106868999+LFNL-scholar@users.noreply.github.com>
2023-12-13 14:20:31 +0800

    Update STL_vector_01.cpp

--- Algorithm-Dec-2023/STL_vector_01.cpp
+++ Algorithm-Dec-2023/STL_vector_01.cpp
@@ -1,4 +1,3 @@
-// vector存放内置数据类型
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -8,6 +7,7 @@ void printArry(int val){
     cout<<val<<" ";
 }
 
+// vector存放内置数据类型
 void test01(){
     vector<int> v;
 
@@ -39,7 +39,86 @@ void test01(){
     
 }
 
+class Person{
+public:
+    string m_Name;
+    int m_Age;
+
+    Person(string name,int age):m_Name(name),m_Age(age) {}
+};
+
+// vector存放自定义数据类型
+void test02(){
+    vector<Person> v;
+    Person p1("A",18);
+    Person p2("B",19);
+    Person p3("C",20);
+    Person p4("D",18);
+
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+
+    // 遍历容器中的数据
+    for(vector<Person>::iterator it=v.begin();it!=v.end();it++){
+        // cout<<"Name: "<<(*it).m_Name<<"  Age: "<<(*it).m_Age<<endl;
+        cout<<"Name: "<<it->m_Name<<"  Age: "<<it->m_Age<<endl;
+    }
+}
+
+void test03(){
+    vector<Person*> v;
+    Person p1("A",18);
+    Person p2("B",19);
+    Person p3("C",20);
+    Person p4("D",18);
+
+    v.push_back(&p1);
+    v.push_back(&p2);
+    v.push_back(&p3);
+    v.push_back(&p4);
+
+    // 遍历容器
+    for(vector<Person*>::iterator it=v.begin();it!=v.end();it++){
+        cout<<"Name: "<<(*it)->m_Name<<"  Age: "<<(*it)->m_Age<<endl;
+    }
+}
+
+// vector存放
+void test04(){
+    vector<vector<int>> v;
+
+    vector<int> v1;
+    vector<int> v2;
+    vector<int> v3;
+    vector<int> v4;
+
+    for(int i=0;i<4;i++){
+        v1.push_back(i+1);
+        v2.push_back(i+2);
+        v3.push_back(i+3);
+        v4.push_back(i+4);
+    }
+
+    v.push_back(v1);
+    v.push_back(v2);
+    v.push_back(v3);
+    v.push_back(v4);
+
+    for(vector<vector<int>>::iterator it=v.begin();it!=v.end();it++){
+        for(vector<int>::iterator vit=(*it).begin();vit!=(*it).end();vit++){
+            cout<<*vit<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
 int main(){
     test01();
+    test02();
+    test03();
+    test04();
     return 0;
 }
